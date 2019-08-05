a1a948eb2403d1ebf0c0753d2c367f83d5303047
IrinaAreto <aretoamazon@gmail.com>
2019-08-05 16:04:52 +0300

    Zadacha1

---
+++ Zadacha1/Calculation.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadacha1
+{
+    class Calculation
+    {
+        int[] array;
+        int[] arraySMA;
+        int[] newArray;
+        int n;
+        Random rand = new Random();
+        public Calculation(int n)
+        {
+            this.n = n;
+            array = new int[n];
+        }
+        public void Fill()
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rand.Next(0, 25);
+                Console.Write(array[i] + "  ");
+            }
+        }
+        public void CalculateSMA(int p)
+        {
+            arraySMA = new int[n - p + 1];
+            for (int j = 0; j <= (n - (p - 1)) && j < arraySMA.Length; j++)
+            {
+                int sum = 0;
+                for (int i = j + p - 1; i >= j && i < array.Length; i--)
+                {
+                    sum += array[i];
+                }
+                arraySMA[j] = sum / p;
+            }
+            foreach (int a in arraySMA)
+                Console.Write(a + "  ");
+        }
+        public void CalculateRatio()
+        {
+            newArray = new int[n - 1];
+            for (int i = 0; i < newArray.Length; i++)
+                if (array[i] != 0)
+                    newArray[i] = array[i + 1] / array[i];
+                else
+                    newArray[i] = 0;  
+            foreach (int a in newArray)
+                Console.Write(a + "  ");
+        }
+    }
+}
+
---
+++ Zadacha1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadacha1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите размер массива: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            if (n > 0 && n <= 100)
+            {
+                Calculation c = new Calculation(n);
+                Console.WriteLine("Исходный массив: ");
+                c.Fill();
+                Console.WriteLine("\n" + new string('_', 50));
+
+                Console.WriteLine("Введите период скользящего среднего: ");
+                int p = Convert.ToInt32(Console.ReadLine());
+                if (p > 1 && p < n)
+                {
+                    Console.WriteLine("Скользящее среднее: ");
+                    c.CalculateSMA(p);
+                    Console.WriteLine("\n" + new string('_', 50));
+                }
+                else
+                    Console.WriteLine("Период скользящего среднего должен быть больше 1 и меньше размера массива!");
+
+                Console.WriteLine("Отношение каждого следующего элемента к предыдущему: ");
+                c.CalculateRatio();
+            }
+            else if (n > 100)
+                Console.WriteLine("Слишком большое значение!");
+            else
+                Console.WriteLine("Размер массива должен быть больше нуля!");
+
+            Console.ReadKey();
+        }
+    }
+}
